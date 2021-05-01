225443af8de175d8a1b410479b6db77bff25bb12
PrajjwalSingh108 <74240106+PrajjwalSingh108@users.noreply.github.com>
2021-05-01 15:54:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,12 +7,12 @@ var question;
 var contestant;
 
 function setup(){
-  canvas = createCanvas(850,400);
+  canvas = createCanvas(850,850);
 }
 
 
 function draw(){
-  background("pink");
+  background("cyan");
 
   
 }
